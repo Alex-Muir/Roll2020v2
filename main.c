63dcaf62c5f4ff5b9fc9e69bf57a1a90e27d1aab
Alex Muir <wamuir1212@protonmail.com>
2024-10-26 10:09:22 -0700

    Added two new files, menu.h and menu.c to hold function prototypes and definitions previously in main

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include "config.h"
 #include "dice.h"
 #include "character.h"
-
-void displayMenu(); 
-void getSelection(char*);
-void invalidSelection(int*);
+#include "menu.h"
 
 int main() {
     
@@ -45,41 +42,3 @@ int main() {
     
     return 0;
 }
-
-// display the choice menu
-void displayMenu() {
-
-    printf("  (1) - Roll a percentage.\n");
-    printf("  (2) - Roll some D10s.\n");
-    printf("  (3) - Roll some D6s.\n");
-    printf("  (4) - Roll a combination of D6s and D10s\n");
-    printf("  (5) - Generate a character and equipment.\n");
-    printf("  (6) - Determine damage points after healing.\n");
-    printf("  (0) - Exit\n\n");
-    printf("So, what do you wanna do? ");
-}
-
-// get the selection of the user
-void getSelection(char* selection) {
-
-    scanf(" %c", selection); 
-}
-
-void invalidSelection(int* invalid) {
-
-    (*invalid)++;
-
-    if(*invalid > 4) {
-        printf("\nWhat a waste of time. Come back when you're serious.\n\n");
-        exit(EXIT_FAILURE);
-
-    } else if(*invalid > 2) {
-        printf("\nStop messin' around. Is this a joke to you?\n\n");
-    } else {
-        printf("\nChoom, c'mon... Enter a valid selection.\n\n");
-    }
-    
-}
-
-
-
---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+void displayMenu(); 
+void getSelection(char*);
+void invalidSelection(int*);
+
+#endif
